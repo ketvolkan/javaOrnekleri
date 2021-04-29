20f0c5056a3dec89f575c50521362f647c735d80
Volkan Ket <ketvolkan1@gmail.com>
2021-04-29 23:29:23 +0300

    eklendi

--- gun3Odev/src/gun3Odev/InstructorManager.java
+++ gun3Odev/src/gun3Odev/InstructorManager.java
@@ -3,7 +3,7 @@ package gun3Odev;
 public class InstructorManager extends UserManager {
     @Override
 	public void add(User usr) {
-		System.out.println(usr.getFirstName()+" Eklendi!");
+		System.out.println(usr.getFirstName()+"  Eklendi!");
 	}
     
 }
